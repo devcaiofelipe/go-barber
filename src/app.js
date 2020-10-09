2b910b0b40cadb149e24d7f2f9d577e521a85411
Caio Felipe <devcaiofelipe15@gmail.com>
2020-10-09 10:55:01 -0300

    final commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import express from 'express';
 import 'express-async-errors';
 import routes from './routes.js';
 import Youch from 'youch';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import path from 'path';
 import * as Sentry from "@sentry/node";
 import sentryConfig from './config/sentry';
@@ -22,7 +26,21 @@ class App {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(Sentry.Handlers.tracingHandler());
     this.server.use(express.json());
-    this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
+    this.server.use(helmet());
+    this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+    
+    if(process.env.NODE_ENV !== 'development') {
+      this.server.use(new RateLimit({
+        store: new RateLimitRedis({
+          client: redis.createClient({
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT
+          }),
+        }),
+        windowMs: 1000 * 60 * 15,
+        max: 5,
+      }));
+    };
   };
 
   routes() {
